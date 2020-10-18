e18a277d8870d1401e2fad11106e12337c7bb514
Mårten Nilsson <nilssonmarten@live.se>
2020-10-18 14:41:52 +0200

    Use max_brightness file

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,34 @@
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io::Read;
-use std::io::Write;
-use std::env;
+const MAX_BRIGHNTESS_FILE: &'static str = "/sys/class/backlight/intel_backlight/max_brightness";
+const BRIGHNTESS_FILE: &'static str = "/sys/class/backlight/intel_backlight/brightness";
+
+fn no_file_msg(file_name: &str) -> String {
+    format!("Couldn't read file {}", file_name)
+}
+
+fn read_to_int(file_name: &str) -> i32 {
+    let value_str = std::fs::read_to_string(file_name).expect(&no_file_msg(file_name));
+    value_str.trim().parse().expect("Could not parse value")
+}
+
+fn brightness_diff(max_brightness: i32, mode: &str) -> i32 {
+    let value = max_brightness / 100;
+    match mode {
+        "inc" => value,
+        "dec" => -value,
+        _ => 0
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
+    let mode: &str = &args[1];
 
-    let target: &str = &args[1];
+    let max_brightness = read_to_int(MAX_BRIGHNTESS_FILE);
+    let brightness = read_to_int(BRIGHNTESS_FILE);
 
-    let mode: &str = &args[2];
+    let diff = brightness_diff(max_brightness, mode);
+    let new_brightness = (brightness + diff).max(0).min(max_brightness);
 
-    let (path, max_brightness) = match target{
-        "screen" => ("/sys/class/backlight/intel_backlight/brightness", 937),
-        "kbd" => ("/sys/class/leds/asus::kbd_backlight/brightness", 3),
-        _ => ("/tmp/derp", 0),
-    };
-
-    //let max_brightness = 937;
-
-    let mut brightness = File::open(path)
-        .expect("Could not open derp file");
-
-    let mut content =  String::new();
-
-    brightness.read_to_string(&mut content).expect("This is weird");
-    println!("Content {}", content);
-
-    let mut val: i32 =  match mode{
-        "inc" => 1,
-        "dec" => -1,
-        _ => 0,
-    };
-
-    let mut min_val = 0;
-
-    if target == "screen" {
-        val *= 50;
-        min_val += 5;
-    }
-
-    let new_brightness = (val + content.trim().parse::<i32>().expect("Wosh")).max(min_val).min(max_brightness);
-    println!("New: {}", new_brightness);
-
-    let mut write_brightness = OpenOptions::new()
-        .write(true)
-        .open(path)
-        .expect("Could not open brightness file");
-    write_brightness.write(&format!("{}", new_brightness).into_bytes()).expect("Unable to write");
+    std::fs::write(BRIGHNTESS_FILE, &format!("{}", new_brightness))
+        .expect("Could not write new brightness");
 }
